a541d716b854773e4185b01bdf25af9e93aa1062
André Ivson <andreivson7@gmail.com>
2023-12-28 14:31:36 -0300

    Add project files.

---
+++ CRUDC#/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CRUDC_.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDC_.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
